3b39f6b8b268ed54d65fde38f3317b4a10a779ef
Joao Bento <jpsbento@gmail.com>
2022-09-21 10:51:39 +0100

    exponential added

--- best_code_ever.py
+++ best_code_ever.py
@@ -7,8 +7,8 @@ number = 4
 def total(price, number):
     return price * number
 
-def div(price, number):
-    return price / number
+def exp(price, number):
+    return price ** number
 
 print("The total amount is %s"%total(price, number))
 
